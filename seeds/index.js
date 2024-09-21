1516ad923f3432de85dba47ff2716f1be03fb00f
ujyllinhybbu <kongdong99@gmail.com>
2024-09-21 17:37:25 -0500

    .

--- seeds/index.js
+++ seeds/index.js
@@ -17,7 +17,7 @@ db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
 });
-
+//
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
 
