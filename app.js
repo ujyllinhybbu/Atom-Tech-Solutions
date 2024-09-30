214b8cb9498794a1fb5ca17454039a3b8ce87266
ujyllinhybbu <kongdong99@gmail.com>
2024-09-30 16:27:03 -0500

    as

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
